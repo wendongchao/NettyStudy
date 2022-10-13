9470126c9b5ad33dc7012bea0630cfc258d9ddb5
wendongchao <dongchao1860369@sina.com>
2022-10-13 23:26:29 +0800

    修改描述

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # NettyStudy
+
+## 首先要做的
+
+把项目lib下的jar把添加到项目中`netty-all-4.1.63.Final.jar`
+
+项目结构->模块->依赖->选择jar->应用
+
+
+
--- src/main/java/com/io/bio/BIOServer.java
+++ src/main/java/com/io/bio/BIOServer.java
@@ -22,10 +22,11 @@ public class BIOServer {
         ServerSocket serverSocket = new ServerSocket(6666);
         System.out.println("服务器启动了");
         while (true) {
-            System.out.println("线程信息id = " + Thread.currentThread().getId() + "名字 = " + Thread.currentThread().getName());
+            System.out.println("主线程信息id = " + Thread.currentThread().getId() + "名字 = " + Thread.currentThread().getName());
             //监听，等待客户端连接
             System.out.println("等待连接....");// 当没有客户端连接时，会一直监听端口
-            final Socket socket = serverSocket.accept();// socket的accept方法会监听连接6666端口的客户端，连上后会生成一个socket对象，作为通信使用
+            // socket的accept方法会监听连接6666端口的客户端，连上后会生成一个socket对象，作为通信使用
+            final Socket socket = serverSocket.accept();
             System.out.println("连接到一个客户端");
             //就创建一个线程，与之通讯(单独写一个方法)
             newCachedThreadPool.execute(new Runnable() {
@@ -41,13 +42,13 @@ public class BIOServer {
     //编写一个handler方法，和客户端通讯
     public static void handler(Socket socket) {
         try {
-            System.out.println("线程信息id = " + Thread.currentThread().getId() + "名字 = " + Thread.currentThread().getName());
+            System.out.println("子线程信息id = " + Thread.currentThread().getId() + "名字 = " + Thread.currentThread().getName());
             byte[] bytes = new byte[1024];
             //通过socket获取输入流，通信对象获取输入流
             InputStream inputStream = socket.getInputStream();
             //循环的读取客户端发送的数据
             while (true) {
-                System.out.println("线程信息id = " + Thread.currentThread().getId() + "名字 = " + Thread.currentThread().getName());
+                System.out.println("子线程信息id = " + Thread.currentThread().getId() + "名字 = " + Thread.currentThread().getName());
                 System.out.println("read....");
                 int read = inputStream.read(bytes);// 当客户端不写时，就一直停在这里，占用资源
                 if (read != -1) {
